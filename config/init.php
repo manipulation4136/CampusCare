8f6ed91dfab94af6cdd67236383ca67a223a6c4d
manipulation4136 <manipulationmastery01@gmail.com>
2026-01-12 22:16:21 +0530

    Update init.php

--- config/init.php
+++ config/init.php
@@ -3,6 +3,10 @@
 ini_set('session.gc_maxlifetime', 3600);
 session_set_cookie_params(3600);
 
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
 require_once __DIR__ . '/env_loader.php';
 loadEnv(__DIR__ . '/../.env');
 
