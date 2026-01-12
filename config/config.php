637cb966c10ab696960e556f3ec6fcd4a4ea7df0
manipulation4136 <manipulationmastery01@gmail.com>
2026-01-12 22:13:20 +0530

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,11 +1,7 @@
 <?php
-// Manual Base URL Configuration
-define('BASE_URL', 'https://campuscare-1bjl.onrender.com/');
+define('BASE_URL', 'http://localhost/website/htdocs/');
 
 // Define Base Path
 if (!defined('BASE_PATH')) {
     define('BASE_PATH', dirname(__DIR__) . '/');
 }
-?>
-
-
