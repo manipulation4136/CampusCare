55c03d29882c020cdca19fa6e50fb981ea3d308b
manipulation4136 <manipulationmastery01@gmail.com>
2026-02-08 19:17:11 +0530

    Update header.php

--- views/partials/header.php
+++ views/partials/header.php
@@ -196,7 +196,7 @@ window.addEventListener('click', function(event) {
               
               <div class="dropdown-divider"></div>
 
-              <a href="<?= BASE_URL ?>includes/logout.php" class="dropdown-item" style="color: #ff6b6b;">
+              <a href="<?= BASE_URL ?>logout" class="dropdown-item" style="color: #ff6b6b;">
                   🚪 Exit
               </a>
           </div>
